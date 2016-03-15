d6d97f2ceb1afb78714d4deea6105a05414ec666
Matthew Demas <mattdemas@virginia.edu>
2016-03-15 14:45:05 -0400

    Changed variance to std. deviation; adjusted trend function to accept x-values as y-values.

--- MD/FeaturesTS.m
+++ MD/FeaturesTS.m
@@ -1,4 +1,4 @@
-function [ts_mean, ts_med, ts_min, ts_max, ts_var, ts_trend,...
+function [ts_mean, ts_med, ts_min, ts_max, ts_std, ts_trend,...
     ts_trend_slope] = FeaturesTS(x_values, y_values)
 %FeaturesTS Given x and y values, the function returns a series of features
 %of interest.
@@ -18,11 +18,12 @@ function [ts_mean, ts_med, ts_min, ts_max, ts_var, ts_trend,...
     ts_max = max(y_values);
     
     % Get var
-    ts_var = var(y_values);
+    ts_std = std(y_values);
     
     % Get trend
     [ts_trend, ts_trend_slope] =...
-        GetTrend(x_values, y_values);
+        GetTrend(y_values, y_values);
+%     GetTrend(x_values, y_values);
 
 end
 
