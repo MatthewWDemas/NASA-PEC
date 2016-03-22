df340417a8b20a41a0dd5f514e864f8b647ced39
Matthew Demas <mattdemas@virginia.edu>
2016-03-22 17:45:16 -0400

    Included code to take in output vector.

--- NN/PerformOrg.m
+++ NN/PerformOrg.m
@@ -1,24 +1,24 @@
 function [ output] = PerformOrg(Data,Test,S,Time,Offset)
 % addpath('~/Box Sync/Nasa Flight Data/DataAnalysis_Matt/NASA-PEC-MATLAB/')
 
-
 % Data = Perform{3}{2};
 % Test = 3;
 % S = 48;
 % Time = ;
 % Offset = ;
 
-
 Offset=-1*Offset;
-output=zeros(length(Time)-1,12)*nan;
+input_output=zeros(length(Time)-1,12)*nan;
+output=zeros(length(Time)-1,36)*nan;
+
 switch Test
-%---------------- SIM ------------------------------
+    %---------------- CFT ------------------------------
     case 1
         output(:,1)=Data(2:end,S);
-%---------------- CogScreen ------------------------
+    %---------------- CogScreen ------------------------
     case 2
         output=output;
-%---------------- MATB -----------------------------
+    %---------------- MATB -----------------------------
     case 3
         % Construct output vectors for each subscore measure
         % Use WindowedSubscoreFeatureExtractorMATB
@@ -26,19 +26,19 @@ switch Test
         % OUTPUT: 5 rows of vector of length 12 that represent the 6
         % features extracted for both the HP measure value and the
         % derivative of the measure value.
-%         EXCLUDED: Composite and Room Air Trials
-%         Comm
+        % EXCLUDED: Composite and Room Air Trials
+        % Comm 
         output_comm = WindowedSubscoreFeatureExtractorMATB(...
-            Data{1}, S, Time, Offset);
-%         ResMan
+            Data{1}, S, Time, Offset, input_output);
+        % ResMan
         output_resman = WindowedSubscoreFeatureExtractorMATB(...
-            Data{2}, S, Time, Offset);
-%         Track
+            Data{2}, S, Time, Offset, input_output);
+        % Track
         output_track = WindowedSubscoreFeatureExtractorMATB(...
-            Data{3}, S, Time, Offset);
+            Data{3}, S, Time, Offset, input_output);
         
         output = horzcat(output_comm, output_resman, output_track);
-%---------------- SIM ------------------------------
+    %---------------- SIM ------------------------------
     case 4
         output=output;
 end
