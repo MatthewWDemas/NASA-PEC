e7b5839f343a922b3b230af61df21afc55d71d38
Matthew Demas <mattdemas@virginia.edu>
2016-03-22 14:32:18 -0400

    Transferred time index search functionality.

--- NN/PerformOrg.m
+++ NN/PerformOrg.m
@@ -1,97 +1,32 @@
 function [ output] = PerformOrg(Data,Test,S,Time,Offset)
 % addpath('~/Box Sync/Nasa Flight Data/DataAnalysis_Matt/NASA-PEC-MATLAB/')
+
+% Data = 
+
 Offset=-1*Offset;
 output=zeros(length(Time)-1,12)*nan;
 switch Test
-    
-    %Case for CFT
+%---------------- SIM ------------------------------
     case 1
         output(:,1)=Data(2:end,S);
-        %Case for Cogn Screen
+%---------------- CogScreen ------------------------
     case 2
         output=output;
-        %Case for Matb
+%---------------- MATB -----------------------------
     case 3
-        Temp=Data(2:end,S);
-        Temp=Temp(~isnan(Temp));
-        if ~isempty(Temp)
-            % Sampling at 5 seconds: This is the time interval.
-            DT=length(Temp)*5;
-            % if there is no experimental marker we assume 60 seconds
-            if isnan(Offset)
-                Offset=60;
-            end
-            TDiff=DT-(Time(end)+Offset); % Time Difference;
-            if TDiff>0
-                OffsetIndex=ceil(Offset/5);
-                if OffsetIndex<1
-                    OffsetIndex=1;
-                end
-                Data=Data(OffsetIndex:end);
-                IndexTime=Time/5;
-                for i=1:length(IndexTime)-1
-                    % This is a hack right now...the GetTrend
-                    % function requires x-values for plotting
-                    % functionality only. It returns the
-                    % difference between the
-                    x_vals = Temp(IndexTime(i)+1:IndexTime(i+1));
-                    y_vals = Temp(IndexTime(i)+1:IndexTime(i+1));
-                    dy_val_dt = diff(y_vals);
-                    [output(i,1), output(i,2), output(i,3), ...
-                        output(i,4), output(i,5), ~, ...
-                        output(i,6)] = ...
-                        FeaturesTS(x_vals, y_vals);
-                    [output(i,7), output(i,8), output(i,9), ...
-                        output(i,10), output(i,11), ~, ...
-                        output(i,12)] = ...
-                        FeaturesTS(dy_val_dt, dy_val_dt);
-                end
-            else
-                OffsetIndex=floor(Offset+TDiff)/5;
-                if OffsetIndex<1
-                    OffsetIndex=1;
-                end
-                Data=Data(OffsetIndex:end);
-                IndexTime=Time/5;
-                % This forloop does the time segment increments
-                for i=1:length(IndexTime)-1
-                    if i==(length(IndexTime)-1)
-                        x_vals = Temp(IndexTime(i)+1:end);
-                        y_vals = Temp(IndexTime(i)+1:end);
-                        dy_val_dt = diff(y_vals);
-                        [output(i,1), output(i,2), output(i,3), ...
-                            output(i,4), output(i,5), ~, ...
-                            output(i,6)] = ...
-                            FeaturesTS(x_vals, y_vals);
-                        [output(i,7), output(i,8), output(i,9), ...
-                            output(i,10), output(i,11), ~, ...
-                            output(i,12)] = ...
-                            FeaturesTS(dy_val_dt, dy_val_dt);
-                    else
-                        x_vals = Temp(IndexTime(i)+1:IndexTime(i+1));
-                        y_vals = Temp(IndexTime(i)+1:IndexTime(i+1));
-                        dy_val_dt = diff(y_vals);
-                        [output(i,1), output(i,2), output(i,3),...
-                            output(i,4), output(i,5), ~,...
-                            output(i,6)] = ...
-                            FeaturesTS(x_vals, y_vals);
-                        [output(i,7), output(i,8), output(i,9), ...
-                            output(i,10), output(i,11), ~, ...
-                            output(i,12)] = ...
-                            FeaturesTS(dy_val_dt, dy_val_dt);
-                    end
-                end
-                
-            end
-        else
-            output=output;
-        end
-        %Case for SIM
+        % Construct output vectors for each subscore measure
+        % Use WindowedSubscoreFeatureExtractorMATB
+        % INPUT: Data, S (Subject), Time, Offset
+        % OUTPUT: 5 rows of vector of length 12 that represent the 6
+        % features extracted for both the HP measure value and the
+        % derivative of the measure value.
+        Composite
+        Comm
+        ResMan
+        Track
+%---------------- SIM ------------------------------
     case 4
         output=output;
 end
-
-
-
 end
 
