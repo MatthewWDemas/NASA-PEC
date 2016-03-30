5f4640132ad96c0a6012cc81ea8d264e438f4cfe
Matthew Demas <mattdemas@virginia.edu>
2016-03-30 12:39:03 -0400

    Added code to find rows that were extracted from DataM feature set and add the new values to the empty matrix location created outside of the loop.

--- CalculateScaledHPFeatures.m
+++ CalculateScaledHPFeatures.m
@@ -8,24 +8,54 @@
 load('../DataExportMATLAB/DataMatrixSeta_ZScoreFullTime_M3_2016_03_24.mat');
 % Get only MATB and non-Room Air trials
 
+FeatArr = DataM(DataM(:,38) ~= 1 & DataM(:,40) == 3,:);
+
+FeatArr = horzcat(FeatArr, nan*ones(size(FeatArr(:, 43:78))));
+
+% Set Case Statements Here:
+% 
+% Should use test mode (values set outside of loop)?
+test_mode = 0;
+% Should use which method of individual scaling (1, 2, 3)?
+use_method = 3;
+
+switch test_mode
+    case 1
+        i = 8;
+        j = 3;
+end
 
 % Cycle through each participant
 for i = 1:49
     % Select rows associated with participant i
-    tmp = 
+    tmpSubj = FeatArr(FeatArr(38,:) == i);
     % Cycle through each run type (non-hypoxic/hypoxic)
     for j = 2:3
         % Select rows associated with participant i and trial j
-        % Save value of the first time instance for participant i, trial j
+        tmp = FeatArr(FeatArr(:,39) == i & FeatArr(:,38) == j, 43:78);
+%         [tmp_locx, tmp_locy, ~] = find(FeatArr(FeatArr(:,39) == i & FeatArr(:,38) == j, :));
+        % Save value of the first row time instance for participant i, trial j
+        tmp_1 = repmat(tmp(1,:), 5, 1);
+        
         % Change array block of HP features (43:78) according to the
         % methods listed below.
-        switch method
+        switch use_method
             case 1
-                
+                B = tmp./tmp_1;
+            case 2
+                B = (tmp - tmp_1)./tmp_1;
+            case 3
+                B = tmp - tmp_1;
+        end
 %         % Cycle through each time instance
 %         for k = 1:5
 %             
 %         end
+        TF = ismember(FeatArr(:,43:78),tmp, 'rows') ;
+%         if sum(TF) > 5
+%             return
+%         end
+        FeatArr(TF, 79:114) = B;
     end
 end
 
