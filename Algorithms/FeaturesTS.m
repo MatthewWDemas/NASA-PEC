acbc956d0c17b2fe3ef5f665926fafffcd96a4f8
Matthew Demas <mattdemas@virginia.edu>
2016-04-08 15:02:46 -0400

    Modified HP feature extractor to calculate median, minimum, maximum, and std dev with detrended y-values.

--- Algorithms/FeaturesTS.m
+++ Algorithms/FeaturesTS.m
@@ -8,22 +8,33 @@ function [ts_mean, ts_med, ts_min, ts_max, ts_std, ts_trend,...
     % Get mean
     ts_mean = mean(y_values);
     
-    % Get median
-    ts_med = median(y_values);
-    
-    % Get min
-    ts_min = min(y_values);
-    
-    % Get max
-    ts_max = max(y_values);
-    
-    % Get std dev
-    ts_std = std(y_values);
+%     % Get median
+%     ts_med = median(y_values);
+%     
+%     % Get min
+%     ts_min = min(y_values);
+%     
+%     % Get max
+%     ts_max = max(y_values);
+%     
+%     % Get std dev
+%     ts_std = std(y_values);
     
     % Get trend
-    [ts_trend, ts_trend_slope] =...
+    [ts_trend, ts_trend_slope, y_values_detrended] =...
         GetTrend(y_values, y_values);
 %     GetTrend(x_values, y_values);
 
+    % Get detrended median
+    ts_med = median(y_values_detrended);
+    
+    % Get detrended min
+    ts_min = min(y_values_detrended);
+    
+    % Get detrended max
+    ts_max = max(y_values_detrended);
+    
+    % Get detrended std dev
+    ts_std = std(y_values_detrended);
 end
 
