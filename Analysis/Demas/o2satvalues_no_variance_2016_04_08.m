cf8d4e11d0065812c1fe298cdee3b563f4f2a7aa
Matthew Demas <mattdemas@virginia.edu>
2016-04-08 15:49:02 -0400

    Code to verify issue with O2 saturation readings.

---
+++ Analysis/Demas/o2satvalues_no_variance_2016_04_08.m
@@ -0,0 +1,11 @@
+
+%% O2 Saturation Values Do Not Change
+% Subject 6's O2 Sat values did not change for some trials.
+
+DataM(DataM(:,39) == 6, [39 40 38 37 36])
+
+%%
+% 
+% * All trials that do not change are "Room Air" trials.
+% * These will be removed and are not an issue at the moment.
+% 
