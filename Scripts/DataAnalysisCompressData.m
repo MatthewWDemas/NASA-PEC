83928cbd331ef42b96e1cbed84c48b06c60e5cdc
Matthew Demas <mattdemas@virginia.edu>
2016-04-11 10:14:21 -0400

    Added script to output cleaned data.

---
+++ Scripts/DataAnalysisCompressData.m
@@ -0,0 +1,101 @@
+clear all 
+close all 
+clc 
+
+%load DataM_3_feat_version_v1_2016_04_04
+load DataMatrixSeta_ZScoreFullTime_M3_indiv_2016_04_08
+Data=DataM_3_feat_version; 
+clear DataM_3_feat_version; 
+
+%-------------------------------------
+Indictors=Data(:,[39,41,40,38,37]);  % Subject , Protocol , Task(Matb,sim,Cft), Hypoxic, Time
+%----- Removing the first minute ------
+TimeRem=~(Indictors(:,5)==1);  % Indictor for first minute
+AntiData=Data(~TimeRem,:);
+Data=Data(TimeRem,:);
+Indictors=Indictors(TimeRem,:); % Reform Indictors 
+% ----- Removal of Subjects ----------
+TimeRem=~(Indictors(:,1)==12);  % Indictor Subject 15 ..Participate 12
+Data=Data(TimeRem,:);
+AntiData=[AntiData;Data(~TimeRem,:)];
+%-------------------------------------
+% Removing Inf ,-Inf and NaN 
+InfData=~( (sum(Data==Inf,2))+ (sum(Data==-Inf,2)) + sum(isnan(Data'))' );   %indictor for infinity
+Data=Data(InfData,:);  % Remove All Infinity
+AntiData=[AntiData;Data(~InfData,:)];
+%-------------------------------------
+% What was removed 
+%-------------------------------------
+
+
+
+PhysioA=Data(:,[1:34,36,42,79:84,85:108])+.0001; 
+PhysioB=Data(:,[109:144,79:84,181:204])+.0001;
+PhysioC=Data(:,[205:240,79:84,277:300])+.0001;
+
+HP_A=Data(:,43:78)+.0001;
+HP_B=Data(:,145:180)+.0001;
+HP_C=Data(:,241:276)+.0001;
+
+
+
+
+save('InputOutput_4_8_16')
+% 
+% % 
+% % Metric=21;
+% % %----------- -----------------------
+% % for i=1:length(PhysioC(1,:))
+% %     figure 
+% % scatter(HP_B(:,Metric),PhysioC(:,i))
+% % end 
+% 
+% 
+% for Metric=1:34
+% %Metric=30;
+% %Output=abs(log(HP_A(:,Metric).*HP_A(:,Metric+1).*HP_A(:,Metric+3)));
+% Output=((HP_A(:,Metric).*HP_A(:,Metric+2)));
+% 
+% %-----------Quantile Normalization -----------------------
+% NN=zscore(([(PhysioA),Output])); 
+% 
+%     for i=1:length(PhysioA(1,:))
+%   %   figure 
+%    %  scatter(NN(:,end),NN(:,i))
+%     II=zscore(NN(:,end))<0;
+%     p(i,Metric)=ttest2(NN(~II,i),NN(II,i));
+%     CC(i,Metric)=corr(NN(:,end),NN(:,i));
+%     end 
+% end
+% 
+%  Output=zscore(Output<0);
+% NewData=quantilenorm([PhysioA,Output]);
+% 
+% % Output=kmeans([HP_A(:,25).*HP_A(:,26),HP_A(:,30)],3,'Replicates',400);
+% % NewData=([PhysioA(:,[51,53,56:58]),Output]);
+% % 
+% % figure; 
+% % hist(Output)
+% 
+% % % Number of Significant Data Sets
+% % figure;
+% % plot(sum(p))
+% % title('Number of Significant Data Sets')
+% % xlabel('Metrics')
+% % ylabel('Number of Significant P Values')
+% % 
+% % % Correlations Over Metrics
+% figure;
+% plot(CC,'DisplayName','CC')
+% title('Correlations Over Metrics (each trend is a metric)')
+% ylabel('Correlations Coefficients')
+% xlabel('Physiological Varibles')
+% max(CC(:,:))
+% min(CC(:,:))
+% 
+% 
+% 
+% 
+% % %% Output=zscore(HP_A(:,Metric))<0;
+% % Output=kmeans([HP_A(:,25).*HP_A(:,26),HP_A(:,30)],4,'Replicates',400);
+% % NewData=([PhysioA,Output]);
